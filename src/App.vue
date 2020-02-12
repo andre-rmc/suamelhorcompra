187ea1178b1592de609bb403041cfd11a31b8bcc
Andre Basilato <andre.basilato@redemelhorcompra.com.br>
2020-02-12 10:40:41 -0300

    subindo projeto exemplo

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,27 @@
 <template>
   <div id="app">
-    <HelloWorld/>
+    <HelloWorld />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HelloWorld
   }
-}
+};
 </script>
 
 <style>
+* {
+  padding: 0px;
+  margin: 0px;
+  overflow-y: hidden;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,13 @@
 <template>
-<div class='background'>
-  <div class='header'>
-    REGULAMENTO DA CAMPANHA SUA MELHOR COMPRA É AQUI!
-  </div>
-  <div class='container-fluid'>
-    <div class='row align-items-center'>
-      <div class='col-md-5'>
-        <img src="../assets/Banner.png" class='img-responsive'>
-      </div>
-      <div class='col-md-7 text-box'>
-        <p class='text'>
+  <div class="background">
+    <div class="header">REGULAMENTO DA CAMPANHA SUA MELHOR COMPRA É AQUI!</div>
+    <div class="container-fluid">
+      <div class="row row-content">
+        <div class="col-md-5 img-box">
+          <img src="../assets/Banner.png" class="img-responsive" />
+        </div>
+        <div class="col-md-7 text-box">
+          <p class="text">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin risus tellus, condimentum non velit suscipit, lacinia sagittis felis. Sed sagittis molestie ligula, a blandit turpis varius non. Nam aliquam venenatis purus id auctor. Nullam nec scelerisque arcu. Nam mollis volutpat mattis. Vivamus quis nibh augue. Nullam sit amet gravida dolor. Nullam nisl velit, sodales a efficitur a, faucibus a lectus. Pellentesque nec fermentum magna.
             Donec orci ipsum, ultricies sit amet sapien eget, feugiat pellentesque velit. Etiam maximus pretium libero, a posuere tellus pulvinar vitae. Suspendisse vitae ex elit. Nulla vestibulum ultricies ligula, eu rhoncus felis accumsan a. Vestibulum pharetra consectetur varius. Nulla vestibulum, nibh id consectetur commodo, nisl purus egestas mauris, in pretium nisi turpis ut felis. Donec ultrices magna nec magna finibus, a tincidunt neque porttitor. Phasellus tempor tempor massa et aliquam. Mauris dictum lectus eget ante tincidunt, ac pellentesque massa consectetur. Suspendisse felis lorem, lobortis a luctus hendrerit, pretium in dui. Cras commodo vehicula nunc, in consectetur diam. Quisque et molestie mauris. Sed volutpat metus non sagittis hendrerit. Quisque vitae posuere metus. In finibus ullamcorper nisi eu fermentum. Fusce sit amet arcu augue.
             Curabitur placerat est pretium, condimentum tellus ut, imperdiet nibh. Quisque consectetur, sem ac commodo rhoncus, quam libero egestas nisi, in luctus nisl dolor id mi. In purus velit, commodo quis justo a, pulvinar accumsan massa. Aenean vel porta magna, commodo tempus lectus. Donec porttitor justo id leo fermentum, vitae malesuada est pulvinar. Maecenas posuere sed diam at suscipit. Donec est lorem, feugiat ut arcu tempus, scelerisque blandit dui. Fusce semper sapien condimentum, pellentesque nunc in, semper metus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In quis quam varius, varius diam vitae, pellentesque velit. Sed finibus ultricies arcu sit amet rhoncus. Ut ut molestie nulla, eu ullamcorper mauris. Suspendisse sodales augue massa, non vestibulum nibh congue non. Sed rutrum consectetur massa, quis dignissim quam hendrerit nec.
@@ -30,20 +28,18 @@
             Duis porttitor sem bibendum interdum sodales. Curabitur et placerat enim, vel pellentesque elit. Vivamus fringilla augue eu nunc egestas, eget sodales metus pharetra. Mauris nec viverra lectus. Integer ultricies massa enim, non laoreet magna commodo vitae. Phasellus dapibus sapien id lobortis dapibus. Quisque suscipit, purus at faucibus vulputate, orci enim iaculis sapien, id bibendum metus est et augue. Donec a placerat augue. Suspendisse accumsan rhoncus sapien et tristique. Nullam id efficitur ex. Curabitur consequat imperdiet magna nec luctus.
             Morbi dapibus sollicitudin tellus, sit amet elementum metus placerat eget. Morbi sed pretium massa, sit amet dapibus est. Etiam efficitur purus porta magna faucibus placerat. Vivamus sollicitudin tincidunt dui sit amet pharetra. Cras ut luctus magna. Fusce bibendum dui sed felis consectetur, sed viverra lacus rhoncus. Nam blandit, arcu a pellentesque facilisis, quam justo tempus leo, eu posuere libero ex eget metus. Integer ut urna ultrices, blandit dolor ut, accumsan nibh. Aliquam pretium sapien mauris, et mollis nibh dignissim vitae. Mauris efficitur pharetra est, non volutpat turpis bibendum vel.
             Nunc convallis molestie sapien ut pharetra. Cras non aliquet libero. Praesent ullamcorper faucibus lectus, ut porttitor ante laoreet at. Cras venenatis, enim a elementum fermentum, est sem eleifend arcu, sed hendrerit purus massa sit amet libero. Suspendisse maximus purus a metus euismod, sit amet venenatis leo hendrerit. Phasellus mauris nisi, egestas eget blandit in, lobortis eget lorem. Nulla ut aliquam dolor.
-        </p>
+          </p>
+        </div>
       </div>
     </div>
-  </div> 
-  <footer class='footer'>
-    @Copyright Rede Melhor Compra 2020
-  </footer>
-</div>
+    <footer class="footer">@Copyright Rede Melhor Compra 2020</footer>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-}
+  name: "HelloWorld"
+};
 </script>
 
 <style scoped>
@@ -62,26 +58,40 @@ export default {
   position: absolute;
   z-index: -1;
 }
+.row-content {
+  height: calc(100vh - 6rem);
+}
+
 .header {
+  /* position: absolute; */
   width: 100%;
   height: 3rem;
   line-height: 3rem;
   border-bottom: 1px solid white;
+  font-weight: bold;
 }
+
+.img-box {
+  height: inherit;
+}
+
 .text-box {
   border-left: 1px solid white;
   overflow-y: auto;
   text-align: justify;
-  height: calc(100vh - 6rem);
+  height: inherit;
 }
+
 .text {
   font-weight: 700;
   margin-top: 1rem;
 }
+
 .img-responsive {
   width: 100%;
   height: auto;
 }
+
 .footer {
   bottom: 0;
   width: 100%;
